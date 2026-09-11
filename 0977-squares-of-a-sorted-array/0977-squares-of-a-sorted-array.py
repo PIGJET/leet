9a3e1711a13b0a48122550fa23e5398dccf1ca6b
PIGJET <104162806+PIGJET@users.noreply.github.com>
2026-09-11 18:45:08 -0400

    Time: 19 ms (17.9%), Space: 14.2 MB (74.21%) - LeetHub

---
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def sortedSquares(self, nums):
+        left = 0
+        right = len(nums) -1
+        ans = [0] * len(nums)
+        i = len(nums)-1
+
+
+        while left <= right:
+            if nums[left] ** 2 > nums[right] **2:
+                ans[i] = nums[left] **2
+                left +=1 
+                 
+
+            else:
+                ans[i] = nums[right] ** 2
+                right -= 1
+                 
+            i-=1
+
+        return ans 
+       
+       
